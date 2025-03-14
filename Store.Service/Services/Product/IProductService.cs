455aab63d98012aa9c9f5af751fa1221348ec5fe
Hana Khaled <Hana_khaled99@outlook.com>
2025-03-14 15:35:38 +0200

    Product Service

---
+++ Store.Service/Services/Product/IProductService.cs
@@ -0,0 +1,18 @@
+﻿using Store.Service.Services.Product.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Service.Services.Product
+{
+    public interface IProductService
+    {
+        Task<ProductDetailsDto> GetProductByIdAsync(int? id);
+        Task<IReadOnlyList<ProductDetailsDto>> GetAllProductsAsync();
+        Task<IReadOnlyList<BrandTypeDetailsDto>> GetAllBrandsAsync();
+        Task<IReadOnlyList<BrandTypeDetailsDto>> GetAllTypesAsync();
+
+    }
+}
