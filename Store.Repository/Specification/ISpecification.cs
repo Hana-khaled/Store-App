52d8141d1f7153315f95fe87b7490a5c210a170e
Hana Khaled <Hana_khaled99@outlook.com>
2025-03-18 14:24:25 +0200

    Prepare Specification classes

---
+++ Store.Repository/Specification/ISpecification.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Repository.Specification
+{
+    public interface ISpecification<T>
+    {
+        public Expression<Func<T, bool>> Criteria { get; } //.Where(...), x => x.id == id
+        public List<Expression<Func<T, object>>> Includes { get; }
+
+    }
+}
