05138dcc533fd86522f50611d2c961f6291b9b0f
Hana Khaled <hana.khaled.alsayed@gmail.com>
2025-07-22 16:02:55 +0300

    Current User Details

--- Store.Web/Controllers/AccountController.cs
+++ Store.Web/Controllers/AccountController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Store.Data.Entities.IdentityEntities;
 using Store.Service.HandleResponses;
 using Store.Service.Services.UserService;
 using Store.Service.Services.UserService.Dtos;
@@ -9,10 +12,12 @@ namespace Store.Web.Controllers
     public class AccountController : BaseController
     {
         private readonly IUserService _userService;
+        private readonly UserManager<AppUser> _userManager;
 
-        public AccountController(IUserService userService)
+        public AccountController(IUserService userService, UserManager<AppUser> userManager)
         {
             _userService = userService;
+            _userManager = userManager;
         }
         [HttpPost]
         public async Task<ActionResult<UserDto>> Login(loginDto input)
@@ -34,6 +39,21 @@ namespace Store.Web.Controllers
                 return BadRequest(new CustomException(400, "Email already Exists"));
 
             return Ok(user);
+        }
+
+        [HttpGet]
+        [Authorize]
+        public async Task<ActionResult<UserDto>> GetCurrentUserDetails()
+        {
+            var userId = User?.FindFirst("UserId");
+            var user = await _userManager.FindByIdAsync(userId.Value);
+
+            return new UserDto
+            {
+                Id = Guid.Parse(user.Id),
+                DisplayName = user.DisplayName,
+                Email = user.Email
+            };
         }
     }
 }
